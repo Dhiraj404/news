91e4c24a489791ba5a2a17cfb4feec0aa03d6b17
Dhiraj404 <53922874+Dhiraj404@users.noreply.github.com>
2022-02-05 01:26:11 +0545

    Add files via upload

--- js/mode.js
+++ js/mode.js
@@ -1,16 +1,19 @@
   
-  $(".lastho").click(function(){
-		$(this).text(function(i, v){
+//   $(".lastho").click(function(){
+// 		$(this).text(function(i, v){
 
 
-		   return v === 'dark' ? 'light' : 'dark'
+// 		   return v === 'dark' ? 'light' : 'dark'
             
-		});
+// 		});
 
 		
-  });
+//   });
 
+  $('.lastho').click(function(){
+    $(this).find('i').toggleClass('fa-plus-circle fa-minus-circle' )
 
+});
 
 
 
